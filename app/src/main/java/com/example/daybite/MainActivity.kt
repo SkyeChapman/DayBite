e111bdb6ea1b616ab8ee16558ada8ad63d6c8394
ThomasRuthig <103006869+ThomasRuthig@users.noreply.github.com>
2022-07-06 21:50:46 -0400

    Initial commit for API integration. Unsure if syntax is valid, documentation is limited.

--- app/src/main/java/com/example/daybite/MainActivity.kt
+++ app/src/main/java/com/example/daybite/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.daybite
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        run("http://api.fungenerators.com")
+    }
+
+    private fun run(url: String)
+    {
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object :Callback{
+            override fun onFailure(call:Call, e: IOException){}
+            override fun onResponse(call: Call, response: Response) = println(response.body?.string())
+        })
     }
 }
