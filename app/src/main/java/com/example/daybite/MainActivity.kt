a726992c9392b5962026d5f94b1b8c7386098aa4
Skye Chapman <skchapman@student.fullsail.edu>
2022-07-14 15:59:23 -0700

    needs some adjusting, binding?

--- app/src/main/java/com/example/daybite/MainActivity.kt
+++ app/src/main/java/com/example/daybite/MainActivity.kt
@@ -3,18 +3,60 @@ package com.example.daybite
 //
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import java.lang.ref.PhantomReference
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    //private lateinit var auth : FirebaseAuth
+   // private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //auth = FirebaseAuth.getInstance()
+        //val uid = auth.currentUser?.uid
+        //databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+
+        binding.NextButton.setOnClickListener {
+
+            val firstName = binding.userName.text.ToString()
+            val emailAddress = binding.userEmail.text.ToString()
+            val password = binding.userPassword.text.ToString()
+            val dateOfBirth = binding.userDateOfBirth.text.ToString()
+
+
+            val user = User(firstName, emailAddress, password, dateOfBirth)
+
+           // if(uid != null)
+          //  {
+                //databaseReference.child(uid).setValue(user).addOnCompleteListener {
+
+                   // if(it.isSuccessful)
+                  //  {
+
+                  //  }
+                   // else
+                  //  {
+                      //  Toast.makeText(context: this@MainActivity, text "Failed to update profile", Toast.LENGTH_SHORT).show()
+
+                   // }
+               // }
+            }
+        }
+
+
+
+
     }
 
 
-    var userNameInput : String = "" //username
-    var userEmailInput : String = "" // email
-    var passwordInput : String = "" //password
-    var dateOfBirth : Int = 0 //user dob
+   // var userNameInput : String = "" //username
+   // var userEmailInput : String = "" // email
+   // var passwordInput : String = "" //password
+   // var dateOfBirth : Int = 0 //user dob
 
 
-}
