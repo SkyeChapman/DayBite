db8b406b6355dbd9569e3753f88c48b775522140
Skye Chapman <skyetoplife@gmail.com>
2022-07-14 16:05:11 -0700

    Select Your Interests init com

--- app/src/main/java/com/example/daybite/MainActivity.kt
+++ app/src/main/java/com/example/daybite/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.daybite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Select Your Interests
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
