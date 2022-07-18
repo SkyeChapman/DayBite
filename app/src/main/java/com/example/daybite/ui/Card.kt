28830786406e5072f6713b29e907dbcc06fe9a52
ThomasRuthig <103006869+ThomasRuthig@users.noreply.github.com>
2022-07-18 19:30:14 -0400

    Card class now calls Requests.kt GetCardFact function, Main Activity now creates a new card temporarily. Card will be called when a new card gets added to the screen

--- app/src/main/java/com/example/daybite/ui/Card.kt
+++ app/src/main/java/com/example/daybite/ui/Card.kt
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.JsonNode
 import org.json.JSONObject
 
 class Card (){
+    var mHeader: String = "topic"
+    var mBody :String = "This is the body of the fact"
+
+
     init {
         val requests = Requests()
         requests.GetCardFact("interest")
