988621027099aabaae77e76cc945a19e765469ba
Skye Chapman <skyetoplife@gmail.com>
2022-07-18 22:08:55 -0700

    binding? done?

--- app/src/main/java/com/example/daybite/MainActivity.kt
+++ app/src/main/java/com/example/daybite/MainActivity.kt
@@ -3,60 +3,95 @@ package com.example.daybite
 //
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.SaveInfo
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.savedstate.SavedStateRegistry
 import java.lang.ref.PhantomReference
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: MainActivity
     //private lateinit var auth : FirebaseAuth
-   // private lateinit var databaseReference: DatabaseReference
+    // private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        // binding = MainActivity().binding.
+
+
+        setContentView(R.layout.activity_main)
+
         //auth = FirebaseAuth.getInstance()
         //val uid = auth.currentUser?.uid
         //databaseReference = FirebaseDatabase.getInstance().getReference("Users")
 
-        binding.NextButton.setOnClickListener {
 
-            val firstName = binding.userName.text.ToString()
-            val emailAddress = binding.userEmail.text.ToString()
-            val password = binding.userPassword.text.ToString()
-            val dateOfBirth = binding.userDateOfBirth.text.ToString()
+        //Next Button, create user info
+        val nextButton = findViewById<TextView>(R.id.nextButton)
+        nextButton.setOnClickListener {
+            val userNameInput = findViewById<TextView>(R.id.userName)
+            val userName = userNameInput.text.toString()
+            val emailInput = findViewById<TextView>(R.id.userEmail)
+            val userEmail = emailInput.text.toString()
+            val passwordInput = findViewById<TextView>(R.id.userPassword)
+            val userPassword = passwordInput.text.toString()
+            val dateOfBirthInput = findViewById<TextView>(R.id.userDateOfBirth)
+            val userDateOfBirth = dateOfBirthInput.inputType.toInt()
+            val nightModeInput = findViewById<TextView>(R.id.darkModeSwitch)
+            val userNightModeSwitch = nightModeInput.isActivated
+            val notificationInput = findViewById<TextView>(R.id.notificationsSwitch)
+            val userNotificationSwitch = notificationInput.isActivated
 
 
-            val user = User(firstName, emailAddress, password, dateOfBirth)
+            val userAccount = User(userName, userEmail, userPassword, userDateOfBirth, userNightModeSwitch, userNotificationSwitch)
+            //save userAccount?
+            // android.service.autofill.SaveInfo.SAVE_DATA_TYPE_USERNAME  //?
 
-           // if(uid != null)
-          //  {
-                //databaseReference.child(uid).setValue(user).addOnCompleteListener {
+        }
 
-                   // if(it.isSuccessful)
-                  //  {
 
-                  //  }
-                   // else
-                  //  {
-                      //  Toast.makeText(context: this@MainActivity, text "Failed to update profile", Toast.LENGTH_SHORT).show()
+        //Back Button, returns to previous screen
+        val backButton = findViewById<TextView>(R.id.backButton)
+        backButton.setOnClickListener {
 
-                   // }
-               // }
-            }
+            onBackPressed()
+            //finish()
+
         }
 
 
 
 
+
+            // if(uid != null)
+            //  {
+            //databaseReference.child(uid).setValue(user).addOnCompleteListener {
+
+            // if(it.isSuccessful)
+            //  {
+
+            //  }
+            // else
+            //  {
+            //  Toast.makeText(context: this@MainActivity, text "Failed to update profile", Toast.LENGTH_SHORT).show()
+
+            // }
+            // }
+        }
     }
 
 
-   // var userNameInput : String = "" //username
-   // var userEmailInput : String = "" // email
-   // var passwordInput : String = "" //password
-   // var dateOfBirth : Int = 0 //user dob
+
+
+
+
+
 
 
--- app/src/main/java/com/example/daybite/User.kt
+++ app/src/main/java/com/example/daybite/User.kt
@@ -1,3 +1,3 @@
 package com.example.daybite
 
-data class User(var userName : String ?= null, var userEmail : String ?= null, var userPassword : String ?= null, var userDateOfBirth : Int ?= null )
+data class User(var userName : String ?= null, var userEmail : String ?= null, var userPassword : String ?= null, var userDateOfBirth : Int ?= null, var userNotificationButton : Boolean ?= false, var userNightModeButton : Boolean ?= false )
