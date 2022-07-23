ce6e90158f44007fb2ab98bc50e631703e8d646e
ThomasRuthig <103006869+ThomasRuthig@users.noreply.github.com>
2022-07-22 20:49:12 -0400

    Pushed changes to blurb

--- app/src/main/java/com/example/daybite/Blurb.kt
+++ app/src/main/java/com/example/daybite/Blurb.kt
@@ -1,3 +1,28 @@
-package com.example.daybite
 
-//data class Blurb()
+package com.example.daybite.ui
+
+import android.provider.Settings.Global.getString
+import android.widget.TextView
+import com.example.daybite.R
+import com.example.daybite.Requests.Requests
+import org.w3c.dom.Text
+
+class Blurb (){
+    var mHeader: String = "topic"
+    var mBody : String = "This is the body of the fact"
+    fun GetFactBody(): String {return mBody}
+    fun GetFactInterest():String{return mHeader}
+    fun SetFactBody(body:String)
+    {
+        mBody = body
+    }
+    fun SetFactInterest(topic:String)
+    {
+        mHeader = topic
+    }
+
+    init {
+        val requests = Requests()
+        this.SetFactBody(requests.GetCardFact("interest")!!.asText())
+    }
+}
