8b150371ccd5d5d4cef7adf6c1e0afe485ccdafa
Arrington Perry <perryceo21@hotmail.com>
2022-07-29 17:34:45 -0400

    Logout of firebase

--- app/src/main/java/com/example/daybite/DashBoardFragment.kt
+++ app/src/main/java/com/example/daybite/DashBoardFragment.kt
@@ -6,22 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 /**
  * A simple [Fragment] subclass.
  */
 class DashBoardFragment : Fragment() {
-
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         val view = inflater.inflate(R.layout.fragment_dash_board, container, false)
+
+
         view.findViewById<Button>(R.id.logOUT).setOnClickListener {
+            firebaseAuth.signOut()
+
+            //Switch back to Login screen
             val navReg = activity as Navigator
             navReg.fragNavigation(LoginFragment(), false)
+
+            Toast.makeText(context, "LogOut Successful",Toast.LENGTH_SHORT).show()
         }
         return view
     }
