88931a38c6592c0eea15f246793ed6aef4fba03f
ThomasRuthig <103006869+ThomasRuthig@users.noreply.github.com>
2022-08-09 21:51:21 -0400

    Fixed API data
    used *one line of code* to affix API data to text view

--- app/src/main/java/com/example/daybite/Blurbs/Blurb.kt
+++ app/src/main/java/com/example/daybite/Blurbs/Blurb.kt
@@ -1,16 +1,12 @@
 
 package com.example.daybite.Blurbs
 //code by tom
-import android.provider.Settings.Global.getString
-import android.widget.TextView
-import com.example.daybite.R
 import com.example.daybite.Requests.Requests
-import org.w3c.dom.Text
 
 class Blurb (){
     //members of class
-    var mHeader: String = "topic"
-    var mBody : String = "This is the body of the fact"
+    var mHeader = String()
+    var mBody = String()
     //mutators and accessors
     fun GetFactBody(): String {return mBody}
     fun GetFactInterest():String{return mHeader}
@@ -27,7 +23,7 @@ class Blurb (){
         //create API request
         val requests = Requests()
         //set API fact to Blurb body text
-        this.SetFactBody(requests.GetCardFact("interest")!!.asText())
+        this.SetFactBody(requests.GenerateBlurbFact("fact"))
         //set topic of blurb
         this.SetFactInterest("Temporary Stand-in Interest")
     }
